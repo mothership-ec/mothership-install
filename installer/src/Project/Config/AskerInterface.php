cd9a2a0840696cd979ef0fd8c9252272ed761877
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-04-21 10:56:22 +0100

    add docblock to AskerInterface

--- installer/src/Project/Config/AskerInterface.php
+++ installer/src/Project/Config/AskerInterface.php
@@ -2,7 +2,14 @@
 
 namespace Mothership\Install\Project\Config;
 
-
+/**
+ * Interface AskerInterface
+ * @package Mothership\Install\Project\Config
+ *
+ * @author  Thomas Marchant <thomas@mothership.ec>
+ *
+ * Interface representing a class that asks the user to input their config details
+ */
 interface AskerInterface
 {
 	/**
